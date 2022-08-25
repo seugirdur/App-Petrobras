ff01b904edb74b072194a9bd52b276032742099a
Alexsander <alexsanderoliveirasv@gmail.com>
2022-08-25 11:33:27 -0300

    Início da activity "Relatório do Processo".

---
+++ app/src/main/java/com/example/apppetrobras/RelatorioProcesso.java
@@ -0,0 +1,14 @@
+package com.example.apppetrobras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelatorioProcesso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorio_processo);
+    }
+}

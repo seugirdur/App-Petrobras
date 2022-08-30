b9eb972aaa65f412badf3a02414b02c30eb10ce3
Alexsander <alexsanderoliveirasv@gmail.com>
2022-08-30 11:33:40 -0300

    Start of Floating Action Button.

--- app/src/main/java/com/example/apppetrobras/RelatorioProcesso.java
+++ app/src/main/java/com/example/apppetrobras/RelatorioProcesso.java
@@ -3,8 +3,13 @@ package com.example.apppetrobras;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class RelatorioProcesso extends AppCompatActivity {
+
+    // Expandable Floating Action Button - Android Studio Tutorial
+    // Time:07:08
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

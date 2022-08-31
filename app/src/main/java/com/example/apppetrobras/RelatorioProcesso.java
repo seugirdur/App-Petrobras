ea77815a1505c5d08ae4e5e34d6e446d8fb5fdb6
Alexsander <alexsanderoliveirasv@gmail.com>
2022-08-31 11:30:32 -0300

    Floating Action Button pronto; Novas text views adicionadas;

--- app/src/main/java/com/example/apppetrobras/RelatorioProcesso.java
+++ app/src/main/java/com/example/apppetrobras/RelatorioProcesso.java
@@ -3,17 +3,89 @@ package com.example.apppetrobras;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RelatorioProcesso extends AppCompatActivity {
 
-    // Expandable Floating Action Button - Android Studio Tutorial
-    // Time:07:08
+    FloatingActionButton add_icon, download_icon, observacoes_icon;
+    Animation fabOpen, fabClose, rotateForward, rotateBackward;
+
+    boolean isOpen = false; // by default it is false
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relatorio_processo);
+
+        add_icon = (FloatingActionButton) findViewById(R.id.add_icon);
+        download_icon = (FloatingActionButton) findViewById(R.id.download_icon);
+        observacoes_icon = (FloatingActionButton) findViewById(R.id.observacoes_icon);
+
+        // animations
+        fabOpen = AnimationUtils.loadAnimation(this, R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(this, R.anim.fab_close);
+
+        rotateForward = AnimationUtils.loadAnimation(this, R.anim.rotate_forward);
+        rotateBackward = AnimationUtils.loadAnimation(this, R.anim.rotate_backward);
+
+        // set the click listener on the main floating action button
+
+        add_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                animateFab();
+
+            }
+        });
+
+        download_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFab();
+                Toast.makeText(RelatorioProcesso.this, "download clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        observacoes_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFab();
+                Toast.makeText(RelatorioProcesso.this, "observações clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    private void animateFab() {
+        if (isOpen) {
+
+            add_icon.startAnimation(rotateBackward);
+            download_icon.startAnimation(fabClose);
+            observacoes_icon.startAnimation(fabClose);
+            download_icon.setClickable(false);
+            observacoes_icon.setClickable(false);
+            isOpen=false;
+
+        }
+        else {
+
+            add_icon.startAnimation(rotateForward);
+            download_icon.startAnimation(fabOpen);
+            observacoes_icon.startAnimation(fabOpen);
+            download_icon.setClickable(true);
+            observacoes_icon.setClickable(true);
+            isOpen=true;
+
+
+        }
+
+    }
+
+
 }

5546159748dd182ce7f37c7c3409bfe95a2c5303
Dev <gustavomurilo012@gmail.com>
2022-09-01 10:07:34 -0300

    foi splashscreen agora muda pra tab

--- app/src/main/java/com/example/apppetrobras/SplashScreenActivity.java
+++ app/src/main/java/com/example/apppetrobras/SplashScreenActivity.java
@@ -19,7 +19,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreenActivity.this, TabActivity.class);
                 startActivity(intent);
                 finish();
             }

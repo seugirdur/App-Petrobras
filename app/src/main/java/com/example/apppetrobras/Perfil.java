ddf500604a333995ee39cd617bb021f70e3e18b1
Grazi <gamagraziellaferreira@gmail.com>
2022-09-28 09:45:14 -0300

    função do perfil dionisio arrumado

---
+++ app/src/main/java/com/example/apppetrobras/Perfil.java
@@ -0,0 +1,44 @@
+package com.example.apppetrobras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Perfil extends AppCompatActivity {
+
+
+    TextView nomecompleto,num_tel,email1,num_chave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        String nome = sharedPreferences.getString("nome", "");
+        String tel = sharedPreferences.getString("tel", "");
+        String email = sharedPreferences.getString("email", "");
+        String chave = sharedPreferences.getString("chave", "");
+
+        nomecompleto = findViewById(R.id.nomecompleto);
+        nomecompleto.setText(nome);
+
+        num_tel = findViewById(R.id.num_tel);
+        num_tel.setText(tel);
+
+        email1 = findViewById(R.id.email);
+        email1.setText(email);
+
+        num_chave = findViewById(R.id.num_chave);
+        num_chave.setText(chave);
+
+
+
+
+    }
+}

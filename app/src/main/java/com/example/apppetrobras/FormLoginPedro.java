8f297eed76fab128aaef2267e191d2f0029e6483
pedrante-bg <110915360+pedrante-bg@users.noreply.github.com>
2022-09-29 09:19:07 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/apppetrobras/FormLoginPedro.java
@@ -0,0 +1,29 @@
+package com.example.telaum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ms.square.android.expandabletextview.ExpandableTextView;
+//import android.widget.ImageView;
+
+
+public class FormLoginPedro extends AppCompatActivity {
+
+    //ImageView logo,appName,splashImg;
+    //LottieAnimationView LottieAnimationView;
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login_pedro);
+
+        ExpandableTextView textView=findViewById(R.id.expand_text_view);
+        textView.setText(getString(R.string.dummyText));
+
+        ///logo = findViewById(R.id.logo);
+        //logo = findViewById(R.id.logo);
+        //logo = findViewById(R.id.logo);
+
+    }
+}

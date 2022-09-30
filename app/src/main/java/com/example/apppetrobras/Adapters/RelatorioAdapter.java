125f87b11a8377cc5a2b439deebfd4ab62c19c34
Isabelli Ferreira <isabelli.ferreira@ifsp.edu.com.br>
2022-09-30 10:06:31 -0300

    Atualizações para ajustar

--- app/src/main/java/com/example/apppetrobras/Adapters/RelatorioAdapter.java
+++ app/src/main/java/com/example/apppetrobras/Adapters/RelatorioAdapter.java
@@ -1,25 +1,59 @@
-package com.example.apppetrobras.Adapters;
+package com.example.apppetrobras;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RelatorioAdapter extends RecyclerView.Adapter {
+import com.example.apppetrobras.Objects.EtapasRelatorioObj;
+
+import java.util.List;
+
+public class adapter extends RecyclerView.Adapter<adapter.MyViewHolder> {
+    private List<EtapasRelatorioObj> listaRel;
+
+    public adapter(List<EtapasRelatorioObj> lista){
+        this.listaRel = lista;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_relatorio, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        EtapasRelatorioObj passos = listaRel.get(position);
+
+        holder.titulo.setText(passos.getTitulo());
+        holder.subtitulo.setText(passos.getSubtitulo());
+        holder.confirmado.setImageResource(passos.getImagem());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaRel.size();
     }
-} []{
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView titulo;
+        TextView subtitulo;
+        ImageView confirmado;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titulo = itemView.findViewById(R.id.txtTitulo);
+            subtitulo = itemView.findViewById(R.id.txtSubtitulo);
+            confirmado = itemView.findViewById(R.id.imgConfirmado);
         }
+    }
+}

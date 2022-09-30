3037e611aae15b8392d5fe2ae5dab74105ca8d4c
Isabelli Ferreira <isabelli.ferreira@ifsp.edu.com.br>
2022-09-30 09:48:26 -0300

    Atualizações para ajustar

---
+++ app/src/main/java/com/example/apppetrobras/Adapters/RelatorioRecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.apppetrobras.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.apppetrobras.Objects.EtapasRelatorioObj;
+import com.example.apppetrobras.R;
+
+import java.util.List;
+
+public class RelatorioRecyclewViewAdapter extends RecyclerView.Adapter<RelatorioRecyclewViewAdapter.MyViewHolder> {
+    private List<EtapasRelatorioObj> listaRel;
+
+    public RelatorioRecyclewViewAdapter(List<EtapasRelatorioObj> lista){
+        this.listaRel = lista;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_relatorio, parent, false);
+
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        EtapasRelatorioObj passos = listaRel.get(position);
+
+        holder.titulo.setText(passos.getTitulo());
+        holder.subtitulo.setText(passos.getSubtitulo());
+        holder.confirmado.setImageResource(passos.getImagem());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaRel.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView titulo;
+        TextView subtitulo;
+        ImageView confirmado;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titulo = itemView.findViewById(R.id.txtTitulo);
+            subtitulo = itemView.findViewById(R.id.txtSubtitulo);
+            confirmado = itemView.findViewById(R.id.imgConfirmado);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/apppetrobras/Objects/EtapasRelatorioObj.java
@@ -0,0 +1,4 @@
+package com.example.apppetrobras.Objects;
+
+public class EtapasRelatorioObj {
+}

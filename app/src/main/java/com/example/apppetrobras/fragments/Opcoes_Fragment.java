9c201c6431f41621afd3845688bf2b40f5039de7
Pedro de Formiga <pedrohborgesborges@gmail.com>
2022-10-05 08:27:46 -0300

    Revert "MEU DEUS É SO AATE SEXTA!!!!!!"
    This reverts commit e84fb619

--- app/src/main/java/com/example/apppetrobras/fragments/Opcoes_Fragment.java
+++ app/src/main/java/com/example/apppetrobras/fragments/Opcoes_Fragment.java
@@ -1,22 +1,39 @@
 package com.example.apppetrobras.fragments;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.Navigations.Tabs;
+import com.example.apppetrobras.Activities.Cadastro;
+import com.example.apppetrobras.Activities.Login;
+import com.example.apppetrobras.Objects.ProblemasObj;
+import com.example.apppetrobras.Activities.Solucoes;
 import com.example.apppetrobras.R;
+import com.example.apppetrobras.Adapters.RecyclerViewAdapter;
 
-public class Opcoes_Fragment extends Fragment implements View.OnClickListener {
+import java.util.ArrayList;
 
-    private ImageButton btnab1,btnab2,btnab3;
+public class Opcoes_Fragment extends Fragment  {
+
+    Dialog nDialog;
+    ImageButton btnab2;
     View view;
-
 
     // Declaração das variáveis
 
@@ -24,47 +41,30 @@ public class Opcoes_Fragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_opcoes_, container, false);
-        View view = inflater.inflate(R.layout.fragment_opcoes_, container, false);
+        return inflater.inflate(R.layout.fragment_opcoes_, container, false);
 
+        public void abrir_popup (View view){
 
-        btnab2 = view.findViewById(R.id.btnab2);
+            Intent intent = new Intent(this, .class);
+            startActivity(intent);
+        }
 
-        btnab2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Privacidade.class);
-                startActivity(intent);
-
-            }
-        });
-
-        btnab1 = view.findViewById(R.id.btnab1);
-        btnab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Privacidade.class);
-                startActivity(intent);
-            }
-        });
-
-        return view;
-
-
-
-//    @Override
-//    public void onClick(View view) {
+    }
+//        btnab5.setOnClickListener(new View.OnClickListener(){
+//        @Override
+//        public void onClick(View){
 //
-//    }
-//    @Override
-//    public void onClick(View view) {
+//        nDialog.setContentView(R.layout.popup_termos);
+//        nDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        nDialog.show();
 //
 //    }
 
-    }
+//    public void pop_termos() {
+//
+        Intent intent = new Intent(Login.this, Tabs.class);
+        //startActivity(intent);
+//    }
 
-    @Override
-    public void onClick(View view) {
-
     }
-}
+//}

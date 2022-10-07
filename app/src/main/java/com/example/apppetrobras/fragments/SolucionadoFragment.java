73029b76053f3d937f96dffa11334558c17f6478
seugirdur <gabriel.lima137170@gmail.com>
2022-10-07 11:39:55 -0300

    madeemaberto e solucionado

--- app/src/main/java/com/example/apppetrobras/fragments/SolucionadoFragment.java
+++ app/src/main/java/com/example/apppetrobras/fragments/SolucionadoFragment.java
@@ -2,6 +2,7 @@ package com.example.apppetrobras.fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,10 +19,12 @@ import com.example.apppetrobras.Activities.Inicio;
 import com.example.apppetrobras.Activities.Relatorio;
 import com.example.apppetrobras.Activities.Solucoes;
 import com.example.apppetrobras.Adapters.RVAdapterEmAberto;
+import com.example.apppetrobras.Adapters.RVAdapterUserRelatorio;
 import com.example.apppetrobras.Adapters.RecyclerViewAdapter;
 import com.example.apppetrobras.Adapters.RelatorioAdapter;
 import com.example.apppetrobras.Objects.ProblemasObj;
 import com.example.apppetrobras.Objects.AdminObj;
+import com.example.apppetrobras.Objects.RelatorioObj;
 import com.example.apppetrobras.Objects.SolucoesObj;
 import com.example.apppetrobras.R;
 import com.example.apppetrobras.api.RetroFitClient;
@@ -39,6 +42,7 @@ public class SolucionadoFragment extends Fragment implements RecyclerViewIntefac
     // Declaração das variáveis
     private ArrayList<ProblemasObj> dataArrayList;
     List<AdminObj> AdminObjList;
+    List<RelatorioObj> relatorioObjList;
 
     private RecyclerView recyclerview;
     private Context context;
@@ -69,46 +73,49 @@ public class SolucionadoFragment extends Fragment implements RecyclerViewIntefac
 
     @Override
     public void onItemClick(int position) {
-        // Redirecionamento para a tela do problema contendo os títulos das soluções
         Intent intent = new Intent(getActivity(), Relatorio.class);
 
         // Definição de valores que serão redirecionados
-        intent.putExtra("TIPO",3);
-        intent.putExtra("ID_TITULO", dataArrayList.get(position).getId());
+        int idRelatorio = relatorioObjList.get(position).getIdRelatorio();
+        intent.putExtra("idRelatorio", idRelatorio);
         startActivity(intent);
     }
 
-    private void listen(){
+    private String sayMyChave(){
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
-        Call<List<AdminObj>> callme = RetroFitClient
+        String chave = sharedPreferences.getString("chave", "");
+
+        return chave;
+    }
+
+    public void listen(){
+        Call<List<RelatorioObj>> callme = RetroFitClient
                 .getInstance()
                 .getAPI()
-                .getAllRelatoriosClosed();
+                .getRelatorio(sayMyChave());
 
-        callme.enqueue(new Callback<List<AdminObj>>() {
+        callme.enqueue(new Callback<List<RelatorioObj>>() {
             @Override
-            public void onResponse(Call<List<AdminObj>> call, Response<List<AdminObj>> response) {
+            public void onResponse(Call<List<RelatorioObj>> call, Response<List<RelatorioObj>> response) {
                 if (!response.isSuccessful()){
-                    Toast.makeText(getContext(), "wassup", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "wassup", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
                 //tentando guardar num objeto para que seja depois visivel no item_list_admin do dionisio
-                AdminObjList = response.body();
-
-                RVAdapterEmAberto recyclerViewAdapter = new RVAdapterEmAberto(context,
-                        AdminObjList, recyclerViewInteface, R.layout.item_list_admin);
+                relatorioObjList = response.body();
+                RelatorioObj relatorioObj = relatorioObjList.get(0);
+                int id = relatorioObj.getIdRelatorio();
+                RVAdapterUserRelatorio recyclerViewAdapter = new RVAdapterUserRelatorio(context,
+                        relatorioObjList, recyclerViewInteface, R.layout.item_historico);
                 recyclerview.setAdapter(recyclerViewAdapter);
                 recyclerViewAdapter.notifyDataSetChanged();
-
             }
-
             @Override
-            public void onFailure(Call<List<AdminObj>> call, Throwable t) {
-
+            public void onFailure(Call<List<RelatorioObj>> call, Throwable t) {
             }
         });
-
     }
 
 

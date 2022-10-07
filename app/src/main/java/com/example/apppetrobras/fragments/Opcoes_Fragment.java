4888b78a757010249d6302a42783335158d10e19
Pedro de Formiga <pedrohborgesborges@gmail.com>
2022-10-07 11:39:17 -0300

    ACABEI!!!!!! FESTA

--- app/src/main/java/com/example/apppetrobras/fragments/Opcoes_Fragment.java
+++ app/src/main/java/com/example/apppetrobras/fragments/Opcoes_Fragment.java
@@ -2,35 +2,18 @@ package com.example.apppetrobras.fragments;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import com.example.Navigations.Tabs;
-import com.example.apppetrobras.Activities.Cadastro;
-import com.example.apppetrobras.Activities.Login;
 import com.example.apppetrobras.Activities.Privacidade;
-import com.example.apppetrobras.Activities.TelaPrivacidade;
-import com.example.apppetrobras.Objects.ProblemasObj;
-import com.example.apppetrobras.Activities.Solucoes;
 import com.example.apppetrobras.R;
-import com.example.apppetrobras.Adapters.RecyclerViewAdapter;
-
-import java.util.ArrayList;
 
 public class Opcoes_Fragment extends Fragment implements View.OnClickListener {
 
@@ -60,7 +43,9 @@ public class Opcoes_Fragment extends Fragment implements View.OnClickListener {
         btnab1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getActivity(), TelaPrivacidade.class); //abre o termos de uso
+                String url = "https://drive.google.com/file/d/1P39Iel7CKZqv8UF3EkSCCzer_GmJ5_BX/view?usp=sharing";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
                 startActivity(i);
             }
         });

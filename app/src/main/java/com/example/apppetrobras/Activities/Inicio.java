05543ed3271d76adbb62a86346f45f5e31b44ea9
seugirdur <gabriel.lima137170@gmail.com>
2022-10-08 09:46:54 -0300

    tirando backdoor da tela de escolha

--- app/src/main/java/com/example/apppetrobras/Activities/Inicio.java
+++ app/src/main/java/com/example/apppetrobras/Activities/Inicio.java
@@ -2,7 +2,10 @@ package com.example.apppetrobras.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
@@ -15,11 +18,16 @@ public class Inicio extends AppCompatActivity {
 
     Button button2;
     TextView linkTextView;
+    Dialog mDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_inicio);
+        mDialog = new Dialog(this);
+
+
 
         button2 = findViewById(R.id.button2);
 
@@ -34,6 +42,15 @@ public class Inicio extends AppCompatActivity {
 
         //link no botão 'clique aqui' o texto e o link estão no @strings
         linkTextView=findViewById(R.id.textView12);
+        linkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mDialog.setContentView(R.layout.popup);
+                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                mDialog.show();
+            }
+        });
         linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
 
     }

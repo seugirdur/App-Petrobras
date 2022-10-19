0553d03ba26520f960ff6673df2aa82e7fbe9d31
Gustavo-Murilo <110928010+Gustavo-Murilo@users.noreply.github.com>
2022-10-19 08:41:29 -0300

    item historico alterado

--- app/src/main/java/com/example/apppetrobras/Adapters/RVAdapterUserRelatorio.java
+++ app/src/main/java/com/example/apppetrobras/Adapters/RVAdapterUserRelatorio.java
@@ -38,14 +38,15 @@ public class RVAdapterUserRelatorio extends RecyclerView.Adapter<RVAdapterUserRe
         View view = LayoutInflater.from(context).inflate(layout,parent,false);
 
         // Instanciação das variáveis
-        int text1, visual1;
+        int text1, text2, visual1;
 
         // Estrutura de decisão de acordo com o layout(xml) da recyclerview,
         // a partir disso são atribuídos os id's que existem em cada item
         text1 = R.id.txtdata;
+        text2 = R.id.txtsecao;
         visual1 = R.id.incentivoVisual;
         return new RVAdapterUserRelatorio.MyViewHolder(view, recyclerViewInteface,
-                text1, visual1);
+                text1, text2, visual1);
     }
 
     @Override
@@ -57,10 +58,33 @@ public class RVAdapterUserRelatorio extends RecyclerView.Adapter<RVAdapterUserRe
         // Isso é necessário pois as listas recebem diferentes quantidades de valores
 
 
-        if(holder.text1 != null){ holder.text1.setText(data.getDataProcesso());}
+        if(holder.text1 != null){ holder.text1.setText(data.getDataProcesso()); }
+
+        if(holder.text2 != null){
+            String secao;
+            switch(data.getIdSecao()) {
+                case 1:
+                    secao = "Lentidao";
+                    break;
+                case 2:
+                    secao = "Internet";
+                    break;
+                case 3:
+                    secao = "Equipamentos";
+                    break;
+                case 4:
+                    secao = "Outros";
+                    break;
+                default:
+                    secao="Lentidão";
+                    break;
+            }
+            holder.text2.setText(secao);
+        }
+
         if(holder.visual1 != null){
-            if(data.getMade_check().contains("2")){ holder.visual1.setImageResource(R.drawable.ic_check_circle);}
-            else{ holder.visual1.setImageResource(R.drawable.ic_cancel_circle);}
+            if(data.getMade_check().contains("2")){ holder.visual1.setImageResource(R.drawable.ic_check_circle); }
+            else{ holder.visual1.setImageResource(R.drawable.ic_cancel_circle); }
         }
     }
 
@@ -71,16 +95,17 @@ public class RVAdapterUserRelatorio extends RecyclerView.Adapter<RVAdapterUserRe
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
-        TextView text1;
+        TextView text1, text2;
         ImageView visual1;
 
         public MyViewHolder(@NonNull View itemView, RecyclerViewInteface recyclerViewInteface,
-                            int item1, int item2) {
+                            int item1, int item2, int item3) {
             super(itemView);
 
             // Definição de variáveis com os valores dos id's
             text1 = itemView.findViewById(item1);
-            visual1 = itemView.findViewById(item2);
+            text2 = itemView.findViewById(item2);
+            visual1 = itemView.findViewById(item3);
 
             // Define o método onItemClik da interface para cada item da RecyclerView
             moduleOnClick(recyclerViewInteface);
